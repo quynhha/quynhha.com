cecb5e0a832fb01e08752066ba4727707cdbc7d4
Thi Quynh Ha Nguyen <67792778+quynhha@users.noreply.github.com>
2021-01-11 23:50:55 -0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    var bubblyButtons = document.getElementsByClassName("bubbly-button")
+
+    var animateButton = function(e) {
+
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+    
+    e.target.classList.add('animate');
+    setTimeout(function(){
+      e.target.classList.remove('animate');
+    },700);
+  }
+    for (var i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener('mouseover', animateButton, false);
+  }
+})
